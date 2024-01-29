6be697deaedbc86c240f1373e4e240a7a3391733
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-01-29 22:35:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const connectDB = async()=>{
 //middleware
 dotenv.config()
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://web-project-5cyk.vercel.app/",credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
